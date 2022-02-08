460e121cba332f533b866ef54d6dcdc77aa1a5ca
Hatice AKTAŞ <76906114+HaticeAKTAS431@users.noreply.github.com>
2022-02-08 18:55:29 +0300

    feat: added end game screen

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public void NextScene()
+    {
+        Application.LoadLevel(2);
+    }
+
+}
